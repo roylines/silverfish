8bb36519e7e8e8684887393a9fe5965f59b8bfcd
Roy Lines <roy@roylines.co.uk>
2014-12-06 12:20:59 +0000

    added get and set

--- silverfish.js
+++ silverfish.js
@@ -1,15 +1,34 @@
-var restify = require('restify');
+var Cacheman = require('cacheman'),
+  restify = require('restify');
 
-this.server = restify.createServer();
+var server = restify.createServer();
+var cache = new Cacheman('silverfish');
+
+function get(req, res, done) {
+  return cache.get(req.params.key, function(e, v) {
+    if (v === undefined) {
+      res.send(404);
+    } else {
+      res.send(200, v);
+    }
+    return done();
+  });
+}
+
+function put(req, res, done) {
+  return cache.set(req.params.key, req.body, function(e) {
+    res.send(200);
+    return done();
+  });
+}
+
+server.get('/object/:key', get);
+server.put('/object/:key', put);
 
 if (require.main === module) {
-  return this.server.listen(process.env.PORT || 8080, function() {
+  return server.listen(process.env.PORT || 8080, function() {
     console.log('silverfish listening');
   });
 }
 
-exports.listen = function() {
-  this.server.listen.apply(this.server, arguments);
-};
-
-exports.close = this.server.close; 
+module.exports = server;
--- test/silverfish.js
+++ test/silverfish.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect,
+  request = require('request'),
   silverfish = require('../silverfish');
 
 var port = process.env.TESTPORT || 8081;
@@ -9,10 +10,43 @@ describe('listening on ' + port, function() {
   });
 
   after(function(done) {
-    return silverfish.close(done) 
+    silverfish.close();
+    return done();
   });
-  
-  it('should...', function() {
-    expect(true).to.equal(true);
+
+  function url(path) {
+    return 'http://localhost:' + port + path;
+  }
+
+  it('should return 404 for getting a missing object', function(done) {
+    return request
+      .get(url('/object/missing'))
+      .on('response', function(r) {
+        expect(r.statusCode).to.equal(404);
+        return done();
+      });
+  });
+
+  it('should return 200 for putting an object', function(done) {
+    return request
+      .put({
+        url: url('/object/silverfish'),
+        json: {
+          field: 'value'
+        }
+      }, function(e, r, b) {
+        expect(r.statusCode).to.equal(200);
+        return done();
+      });
+  });
+
+  it.skip('should return 200 for getting a valid object', function(done) {
+    return request
+      .get(url('/object/silverfish'))
+      .on('response', function(r) {
+        expect(r.statusCode).to.equal(200);
+        return done();
+      });
+    return done();
   });
 });
